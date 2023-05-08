84cda4bb0c53acea1c2e42bbc84ceb0e7fc5de5d
JESSICA PUTRY <126640804+ratuerror001@users.noreply.github.com>
2023-05-08 18:30:17 +0700

    Update run.py

--- run.py
+++ run.py
@@ -556,7 +556,7 @@ class crack:
 		try:
 			uas = open('ugent.txt','r').read()
 		except (FileNotFoundError,IOError):
-			uas = ("Mozilla/5.0 (Linux; Android 4.2.2; FreeTAB 9000 IPS IC Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Safari/537.36")
+			uas = ("Mozilla/5.0 (Linux; Android 4.4.2; BQS-4007 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36")
 			open('ugent.txt','w').write(uas)
 		
 		return uas 
